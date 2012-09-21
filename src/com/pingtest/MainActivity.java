99d8e1e146041d87dbb1284dac6bf54bb045c11e
ideepblue <deepblue.aesky@gmail.com>
2012-09-21 15:29:29 +0800

    调整一些显示效果

--- src/com/pingtest/MainActivity.java
+++ src/com/pingtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.pingtest;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.content.Context;
@@ -23,10 +24,12 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	private TextView textViewStart;
-	private TextView textViewGsmDbm;
-	private TextView textViewGsmAsu;
-	private TextView textViewLatitude;
-	private TextView textViewLongitude;
+	private TextView textViewSignalStrengths;
+	private TextView textViewPhoneType;
+	private TextView textViewNetworkType;
+	private TextView textViewDataActivity;
+	private TextView textViewLocationStatus;
+	private TextView textViewGps;
 	private TextView textViewPing;
 	private LocationManager locationManager;
 	private LocationListener locationListener;
@@ -57,8 +60,12 @@ public class MainActivity extends Activity {
 		super.onResume();
 		telephonyManager.listen(phoneStateListener,
 				PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000,
-				10, locationListener);
+		telephonyManager.listen(phoneStateListener,
+				PhoneStateListener.LISTEN_DATA_CONNECTION_STATE);
+		telephonyManager.listen(phoneStateListener,
+				PhoneStateListener.LISTEN_DATA_ACTIVITY);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				0, 10, locationListener);
 	}
 
 	private void initUI() {
@@ -108,10 +115,12 @@ public class MainActivity extends Activity {
 			}
 		});
 
-		textViewGsmAsu = (TextView) findViewById(R.id.gsmAsu);
-		textViewGsmDbm = (TextView) findViewById(R.id.gsmDbm);
-		textViewLatitude = (TextView) findViewById(R.id.latitude);
-		textViewLongitude = (TextView) findViewById(R.id.longitude);
+		textViewSignalStrengths = (TextView) findViewById(R.id.signalStrengths);
+		textViewPhoneType = (TextView) findViewById(R.id.phoneType);
+		textViewNetworkType = (TextView) findViewById(R.id.networkType);
+		textViewDataActivity = (TextView) findViewById(R.id.dataActivity);
+		textViewLocationStatus = (TextView) findViewById(R.id.locationStatus);
+		textViewGps = (TextView) findViewById(R.id.gps);
 		textViewPing = (TextView) findViewById(R.id.ping);
 	}
 
@@ -124,18 +133,149 @@ public class MainActivity extends Activity {
 	private void initPhoneListener() {
 		telephonyManager = (TelephonyManager) this
 				.getSystemService(Context.TELEPHONY_SERVICE);
+
+		String str = new String();
+		int phoneType = telephonyManager.getPhoneType();
+		switch (phoneType) {
+		case TelephonyManager.PHONE_TYPE_NONE:
+			str = "PHONE_TYPE_NONE";
+			break;
+		case TelephonyManager.PHONE_TYPE_CDMA:
+			str = "PHONE_TYPE_CDMA";
+			break;
+		case TelephonyManager.PHONE_TYPE_GSM:
+			str = "PHONE_TYPE_GSM";
+			break;
+		/*
+		 * case TelephonyManager.PHONE_TYPE_SIP: str = "PHONE_TYPE_SIP"; break;
+		 */
+		}
+		textViewPhoneType.setText(str);
+
 		phoneStateListener = new PhoneStateListener() {
 			@Override
 			public void onSignalStrengthsChanged(SignalStrength signalStrength) {
 				super.onSignalStrengthsChanged(signalStrength);
-				textViewGsmAsu.setText("GSM_ASU = "
-						+ signalStrength.getGsmSignalStrength());
-				textViewGsmDbm.setText("GSM_dBm = "
-						+ (signalStrength.getGsmSignalStrength() * 2 - 113));
+				String str = new String();
+				if (signalStrength.isGsm()) {
+					str += "isGSM\n";
+					str += "GsmAsu = " + signalStrength.getGsmSignalStrength()
+							+ "\n" + "GsmDbm = "
+							+ (signalStrength.getGsmSignalStrength() * 2 - 113);
+				} else {
+					str += "CdmaDbm = " + signalStrength.getCdmaDbm() + "\n"
+							+ "EvdoDbm = " + signalStrength.getEvdoDbm();
+				}
+				textViewSignalStrengths.setText(str);
+			}
+
+			@Override
+			public void onDataActivity(int direction) {
+				super.onDataActivity(direction);
+				String str = new String();
+				switch (direction) {
+				case TelephonyManager.DATA_ACTIVITY_DORMANT:
+					str += "DATA_ACTIVITY_DORMANT\n";
+					break;
+				case TelephonyManager.DATA_ACTIVITY_IN:
+					str += "DATA_ACTIVITY_IN\n";
+					break;
+				case TelephonyManager.DATA_ACTIVITY_INOUT:
+					str += "DATA_ACTIVITY_INOUT\n";
+					break;
+				case TelephonyManager.DATA_ACTIVITY_NONE:
+					str += "DATA_ACTIVITY_NONE\n";
+					break;
+				case TelephonyManager.DATA_ACTIVITY_OUT:
+					str += "DATA_ACTIVITY_OUT\n";
+					break;
+				}
+				textViewDataActivity.setText(str);
+			}
+
+			@Override
+			public void onDataConnectionStateChanged(int state, int networkType) {
+				super.onDataConnectionStateChanged(state, networkType);
+				String str = new String();
+				switch (state) {
+				case TelephonyManager.DATA_CONNECTED:
+					str += "DATA_CONNECTED\n";
+					break;
+				case TelephonyManager.DATA_CONNECTING:
+					str += "DATA_CONNECTING\n";
+					break;
+				case TelephonyManager.DATA_DISCONNECTED:
+					str += "DATA_DISCONNECTED\n";
+					break;
+				case TelephonyManager.DATA_SUSPENDED:
+					str += "DATA_SUSPENDED\n";
+					break;
+				}
+				switch (networkType) {
+				case TelephonyManager.NETWORK_TYPE_1xRTT:
+					/*
+					 * str += "NETWORK_TYPE_1xRTT\n" break;
+					 */
+				case TelephonyManager.NETWORK_TYPE_CDMA:
+					str += "NETWORK_TYPE_CDMA\n";
+					break;
+				case TelephonyManager.NETWORK_TYPE_EDGE:
+					str += "NETWORK_TYPE_EDGE\n";
+					break;
+				/*
+				 * case TelephonyManager.NETWORK_TYPE_EHRPD: str +=
+				 * "NETWORK_TYPE_EHRPD\n"; break;
+				 */
+				case TelephonyManager.NETWORK_TYPE_EVDO_0:
+					str += "NETWORK_TYPE_EVDO_0\n";
+					break;
+				case TelephonyManager.NETWORK_TYPE_EVDO_A:
+					str += "NETWORK_TYPE_EVDO_A\n";
+					break;
+				/*
+				 * case TelephonyManager.NETWORK_TYPE_EVDO_B: str +=
+				 * "NETWORK_TYPE_EVDO_B\n"; break;
+				 */
+				case TelephonyManager.NETWORK_TYPE_GPRS:
+					str += "NETWORK_TYPE_GPRS\n";
+					break;
+				case TelephonyManager.NETWORK_TYPE_HSDPA:
+					str += "NETWORK_TYPE_HSDPA\n";
+					break;
+				case TelephonyManager.NETWORK_TYPE_HSPA:
+					str += "NETWORK_TYPE_HSPA\n";
+					break;
+				/*
+				 * case TelephonyManager.NETWORK_TYPE_HSPAP: str +=
+				 * "NETWORK_TYPE_HSPAP\n"; break;
+				 */
+				case TelephonyManager.NETWORK_TYPE_HSUPA:
+					str += "NETWORK_TYPE_HSUPA\n";
+					break;
+				/*
+				 * case TelephonyManager.NETWORK_TYPE_IDEN: str +=
+				 * "NETWORK_TYPE_IDEN\n"; break;
+				 */
+				/*
+				 * case TelephonyManager.NETWORK_TYPE_LTE: str +=
+				 * "NETWORK_TYPE_LTE\n"; break;
+				 */
+				case TelephonyManager.NETWORK_TYPE_UMTS:
+					str += "NETWORK_TYPE_UMTS\n";
+					break;
+				case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+					str += "NETWORK_TYPE_UNKNOWN\n";
+					break;
+				}
+				textViewNetworkType.setText(str);
 			}
 		};
 		telephonyManager.listen(phoneStateListener,
 				PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+		telephonyManager.listen(phoneStateListener,
+				PhoneStateListener.LISTEN_DATA_CONNECTION_STATE);
+		telephonyManager.listen(phoneStateListener,
+				PhoneStateListener.LISTEN_DATA_ACTIVITY);
 	}
 
 	private void initLocationListener() {
@@ -145,22 +285,27 @@ public class MainActivity extends Activity {
 			@Override
 			public void onLocationChanged(Location location) {
 
-				textViewLatitude
-						.setText("Latitude = " + location.getLatitude());
-				textViewLongitude.setText("Longitude = "
-						+ location.getLongitude());
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTimeInMillis(System.currentTimeMillis());
+				textViewGps.setText(
+						"Latitude = " + location.getLatitude() + 
+						"\nLongitude = "
+						+ location.getLongitude() + "\nTime = "
+						+ calendar.getTime().getHours() + ":"
+						+ calendar.getTime().getMinutes() + ":"
+						+ calendar.getTime().getSeconds());
 			}
 
 			@Override
 			public void onProviderDisabled(String provider) {
 
-				textViewLongitude.setText("Disabled");
+				textViewLocationStatus.setText("Disabled");
 			}
 
 			@Override
 			public void onProviderEnabled(String provider) {
 
-				textViewLongitude.setText("Enabled");
+				textViewLocationStatus.setText("Enabled");
 			}
 
 			@Override
@@ -168,16 +313,16 @@ public class MainActivity extends Activity {
 					Bundle extras) {
 				switch (status) {
 				case LocationProvider.OUT_OF_SERVICE:
-					textViewLatitude.setText("OUT_OF_SERVICE");
+					textViewLocationStatus.setText("OUT_OF_SERVICE");
 					break;
 				case LocationProvider.AVAILABLE:
-					textViewLatitude.setText("AVAILABLE");
+					textViewLocationStatus.setText("AVAILABLE");
 					break;
 				case LocationProvider.TEMPORARILY_UNAVAILABLE:
-					textViewLatitude.setText("TEMPORARILY_UNAVAILABLE");
+					textViewLocationStatus.setText("TEMPORARILY_UNAVAILABLE");
 					break;
 				default:
-					textViewLatitude.setText("DEFAULT");
+					textViewLocationStatus.setText("DEFAULT");
 					break;
 				}
 
@@ -195,7 +340,7 @@ public class MainActivity extends Activity {
 		}
 
 		// 重要函数，监听数据测试
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000,
-				10, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				0, 10, locationListener);
 	}
 }
