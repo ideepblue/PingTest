6d9e2ae72d40d3d3e5898c0c46d9f9db994d38b7
ideepblue <deepblue.aesky@gmail.com>
2012-09-23 08:15:37 +0800

    遗漏的Actions

---
+++ src/com/pingtest/PingtestActions.java
@@ -0,0 +1,7 @@
+package com.pingtest;
+
+public class PingtestActions {
+	public static final String ACTION_PING = "com.pingtest.action.PING";
+	public static final String ACTION_AGPS_UPDATE = "com.pingtest.action.AGPS_UPDATE";
+	public static final String ACTION_GPS_UPDATE = "com.pingtest.action.GPS_UPDATE";
+}
